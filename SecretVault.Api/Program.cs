73b0e2f3e420d996a5feca0549675fc01b69d255
Sujith Nair <sujithaturservice@gmail.com>
2026-02-20 12:48:03 +0530

    Added health check

--- SecretVault.Api/Program.cs
+++ SecretVault.Api/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAuth(builder.Configuration);
 
+builder.Services.AddHealthChecks();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
@@ -33,4 +34,6 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health");
+
 app.Run();
